fedcd9e3d66ad7ed5c3929488c8108de0ebf1481
Rod Johnson <johnsonroda@gmail.com>
2018-11-21 15:09:28 +0100

    mess up formatting

--- src/main/java/com/dutch/Rene1Controller.java
+++ src/main/java/com/dutch/Rene1Controller.java
@@ -5,16 +5,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class Rene1Controller {
+class Rene1Controller
+
+
+
+
+{
 
 	@GetMapping("/")
-	public String root() {
+	public String 
+		
+		
+		
+		
+		
+		
+		root() {
 		return "Hello Copenhagen Tuesday 16:37! I'm served by " + getClass().getName();
-	}
+}
 
 	@GetMapping("hello/{name}")
 	public String person(@PathVariable String name) {
 		return "Hello " + name + "!";
-	}
-
-}
+	}}
